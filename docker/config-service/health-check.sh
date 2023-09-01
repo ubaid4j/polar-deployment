fe74784dd90fd196633c0cc558c04cccfbf9eced
ubaid4j <ubaidshan007@gmail.com>
2023-09-01 12:38:57 +0500

    - health check

--- docker/config-service/health-check.sh
+++ docker/config-service/health-check.sh
@@ -3,7 +3,7 @@ exec 3<>/dev/tcp/localhost/50100
 
 echo -e "GET /management/health HTTP/1.1\nhost: localhost:50100\n" >&3
 
-timeout --preserve-status 6000 cat <&3 | grep -m 1 status | grep -m 1 UP
+timeout --preserve-status 180 cat <&3 | grep -m 1 status | grep -m 1 UP
 ERROR=$?
 
 exec 3<&-
