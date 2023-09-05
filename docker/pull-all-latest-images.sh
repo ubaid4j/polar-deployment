03e701edcde505058050d9c50aca45ada447b691
ubaid4j <ubaidshan007@gmail.com>
2023-09-05 10:46:38 +0500

    - tracing

--- docker/pull-all-latest-images.sh
+++ docker/pull-all-latest-images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-declare -a microservices=("catalog-service:0.0.4-snapshot" "config-service:0.0.3-snapshot" "dispatcher-service:0.0.2-snapshot" "order-service:0.0.5-snapshot" "edge-service:0.0.4-snapshot")
+declare -a microservices=("catalog-service:0.0.5-snapshot" "config-service:0.0.3-snapshot" "dispatcher-service:0.0.2-snapshot" "order-service:0.0.5-snapshot" "edge-service:0.0.4-snapshot")
 for microservice in "${microservices[@]}"
 do
   docker pull ghcr.io/ubaid4j/"$microservice"
